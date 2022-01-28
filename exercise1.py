044bb432ee494d82ca906284a0ab09c54731ac0f
fabio.vicini <fabio.vicini@polito.it>
2022-01-28 17:58:08 +0100

    add: jupyter project

--- exercise1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy import io, integrate, linalg, signal
-from scipy.sparse.linalg import eigs
-
-eps = np.finfo(float).eps or np.spacing(1)
-
-a = np.arange(1., 0.0-0.2, -0.2)
-
-print(a)
